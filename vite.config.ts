4ae3fb094820b99bc00c4dc780abc5fb8f4877cb
Jose Manuel Alarcon <polilla2802@gmail.com>
2024-05-23 16:19:49 -0500

    Taking port from .env

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,16 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vite";
+import dotenv from "dotenv";
+
+// Load environment variables from .env file
+dotenv.config();
+
+// Get the port from the environment variables, default to 8000 if not set
+const port = process.env.VITE_PORT ? parseInt(process.env.VITE_PORT) : 5173;
 
 export default defineConfig({
-	plugins: [sveltekit()]
+  plugins: [sveltekit()],
+  server: {
+    port: port,
+  },
 });

a50c9c529fe12cbe444503d7e3327ef5b93f9a05
Jose Manuel Alarcon <78869984+polilla2802@users.noreply.github.com>
2024-05-29 20:09:34 -0500

    Build 1.3.0+4
    -Added .env.prod and .env.dev
    -Edits on the package.json scripts for dev, build and prisma for dev and prod builds
    -Added firebaseConfig file to load env variables
    -Edit on the frame_designs seed

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,20 +1,12 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getStorage } from 'firebase/storage';
+import firebaseConfig from '$lib/firebaseConfig';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyAJNf8vVyi25ges_koX7GYGmHhwJD5Hj5Y",
-  authDomain: "cartas-a-bris.firebaseapp.com",
-  projectId: "cartas-a-bris",
-  storageBucket: "cartas-a-bris.appspot.com",
-  messagingSenderId: "312270713162",
-  appId: "1:312270713162:web:c44655ff71a7e15b5411b3",
-  measurementId: "G-PT18HXXCFW",
-};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,35 @@
 import { sveltekit } from "@sveltejs/kit/vite";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import dotenv from "dotenv";
 
 // Load environment variables from .env file
 dotenv.config();
 
 // Get the port from the environment variables, default to 8000 if not set
-const port = process.env.VITE_PORT ? parseInt(process.env.VITE_PORT) : 5173;
+// const port = process.env.VITE_PORT ? parseInt(process.env.VITE_PORT) : 5173;
 
-export default defineConfig({
-  plugins: [sveltekit()],
-  server: {
-    port: port,
-  },
+
+export default defineConfig(({ mode }) => {
+  // Determine the appropriate env file to load based on the mode
+  let env;
+  if (mode === 'development') {
+    env = loadEnv('dev', process.cwd(), '');
+  } else if (mode === 'production') {
+    env = loadEnv('prod', process.cwd(), '');
+  } else {
+    env = loadEnv(mode, process.cwd(), '');
+  }
+
+  // Ensure the port is a number
+  const port = env.VITE_PORT ? parseInt(env.VITE_PORT, 10) : 5173;
+
+  return {
+    plugins: [sveltekit()],
+    define: {
+      'process.env': env,
+    },
+    server: {
+      port: port,
+    },
+  };
 });

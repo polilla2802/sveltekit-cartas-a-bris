d9f8d8951f45ed2545c29053147989784f231cd5
Jose Manuel Alarcon <78869984+polilla2802@users.noreply.github.com>
2024-05-31 18:48:34 -0500

    Created frameTypeEnum
    -Added frameTypeEnum and methods to get the strings

---
+++ src/lib/enums/frames.ts
@@ -0,0 +1,86 @@
+export enum frameTypeEnum {
+	romantic,
+	galactic,
+	funny,
+	monochrome,
+	vintage,
+	modern,
+	classic,
+	nature,
+	minimalist,
+	abstract,
+	beach,
+	christmas,
+	badbunny,
+	sexy
+}
+
+export function getTypeEnumByTypeId(FrameTypeId: number) {
+	switch (FrameTypeId) {
+		case 1:
+			return frameTypeEnum.romantic;
+		case 2:
+			return frameTypeEnum.galactic;
+		case 3:
+			return frameTypeEnum.funny;
+		case 4:
+			return frameTypeEnum.monochrome;
+		case 5:
+			return frameTypeEnum.vintage;
+		case 6:
+			return frameTypeEnum.modern;
+		case 7:
+			return frameTypeEnum.classic;
+		case 8:
+			return frameTypeEnum.nature;
+		case 9:
+			return frameTypeEnum.minimalist;
+		case 10:
+			return frameTypeEnum.abstract;
+		case 11:
+			return frameTypeEnum.beach;
+		case 12:
+			return frameTypeEnum.christmas;
+		case 13:
+			return frameTypeEnum.badbunny;
+		case 14:
+			return frameTypeEnum.sexy;
+		default:
+			return frameTypeEnum.badbunny;
+	}
+}
+
+export function getFrameStringByTypeEnum(FrametypeEnum: frameTypeEnum) {
+	switch (FrametypeEnum) {
+		case frameTypeEnum.romantic:
+			return "Romantic";
+		case frameTypeEnum.galactic:
+			return "Galactic";
+		case frameTypeEnum.funny:
+			return "Funny";
+		case frameTypeEnum.monochrome:
+			return "Monochrome";
+		case frameTypeEnum.vintage:
+			return "Vintage";
+		case frameTypeEnum.modern:
+			return "Modern";
+		case frameTypeEnum.classic:
+			return "Classic";
+		case frameTypeEnum.nature:
+			return "Nature";
+		case frameTypeEnum.minimalist:
+			return "Minimalist";
+		case frameTypeEnum.abstract:
+			return "Abstact"
+		case frameTypeEnum.beach:
+			return "Beach";
+		case frameTypeEnum.christmas:
+			return "Christmas";
+		case frameTypeEnum.badbunny:
+			return "Bad Bunny";
+		case frameTypeEnum.sexy:
+			return "Sexy";
+		default:
+			return "Unknown";
+	}
+}

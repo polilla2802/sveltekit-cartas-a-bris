6a06d1c68989d66d4ac3777073f621750e7de66a
Jose Manuel Alarcon <78869984+polilla2802@users.noreply.github.com>
2024-06-03 18:02:26 -0500

    Env prod test

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
-import { sveltekit } from "@sveltejs/kit/vite";
-import { defineConfig } from "vite";
-import dotenv from "dotenv";
+import { defineConfig } from 'vite';
+import { sveltekit } from '@sveltejs/kit/vite';
+import * as dotenv from 'dotenv';
+import { resolve } from 'path';
 
-// Load environment variables from .env file
-dotenv.config();
+// Load environment variables based on the mode
+const envFile = `.env.${process.env.NODE_ENV}`;
+dotenv.config({ path: resolve(__dirname, envFile) });
 
 // Get the port from the environment variables, default to 8000 if not set
 const port = process.env.VITE_PORT ? parseInt(process.env.VITE_PORT) : 5173;

16b3f8b23d44f72929eccfa4e91ce3f16e95a770
Jose Manuel Alarcon <78869984+polilla2802@users.noreply.github.com>
2024-06-12 12:35:17 -0500

    Prisma changes
    -Se agrego el @updatedAt type
    -Se agrego el API de Users

---
+++ src/routes/api/users/+server.ts
@@ -0,0 +1,93 @@
+import { error, json, type RequestHandler } from '@sveltejs/kit';
+import prisma from '$lib/prisma';
+
+export const GET: RequestHandler = async () => {
+  try {
+    const users = await prisma.user.findMany({
+      include: {
+        frame_finalized: true,
+      },
+    });
+
+    return json({ users }, {
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+      },
+    });
+  } catch (err) {
+    console.error(err);
+    throw error(500, `Failed to retrieve frame_designs records: ${(err as Error).message}`);
+  }
+};
+
+
+export const POST: RequestHandler = async ({ request }) => {
+  // Get the current date
+const today = new Date()
+
+  const data = await request.formData();
+  const userNameValue = data.get('userName');
+  const nameValue = data.get('name');
+  const phoneNumberValue = data.get('phoneNumber');
+  const emailValue = data.get('email');
+  const passwordValue = data.get('password');
+  const genderValue = data.get('gender');
+  const ageValue = data.get('age');
+
+  if (!userNameValue) {
+    throw error(400, 'userName not provided');
+  }
+
+  if (!nameValue) {
+    throw error(400, 'name not provided');
+  }
+
+  if (!emailValue) {
+    throw error(400, 'email not provided');
+  }
+
+  if (!passwordValue) {
+    throw error(400, 'password not provided');
+  }
+
+  if (!ageValue) {
+    throw error(400, 'age not provided');
+  }
+
+  const age = parseInt(ageValue as string);
+  if (isNaN(age)) {
+    throw error(400, 'age must be a valid number');
+  }
+
+  const userName = (userNameValue as string)
+  const name = (nameValue as string)
+  const phoneNumber = (phoneNumberValue as string)
+  const email = (emailValue as string)
+  const password = (passwordValue as string)
+  const gender = (genderValue as string)
+  try {
+
+    const user = await prisma.user.create({
+      data: {
+        userName: userName,
+        name: name,
+        phoneNumber: phoneNumber,
+        email: email,
+        password: password,
+        gender: gender,
+        age: age
+      },
+    });
+
+    return json({ message: 'User uploaded:', user }, {
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+      },
+    });
+
+  } catch (err) {
+    console.error(err);
+    throw error(500, `Upload failed: ${(err as Error).message}`);
+  }
+};
+

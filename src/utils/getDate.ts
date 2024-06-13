cecabd27a18a62e44090097a6d34b2260f7154e3
Jose Manuel Alarcon <78869984+polilla2802@users.noreply.github.com>
2024-06-12 20:21:02 -0500

    fixed formatDate

--- src/utils/getDate.ts
+++ src/utils/getDate.ts
@@ -1,11 +1,16 @@
 export function formatDate(dateString: string): string {
 	const date = new Date(dateString);
+    
+	// Check if the parsed date is valid
 	if (isNaN(date.getTime())) {
-		console.error(`Invalid date: ${dateString}`);
-		return dateString; // Return the original string if it's not a valid date
+	    console.error(`Invalid date: ${dateString}`);
+	    return dateString; // Return the original string if it's not a valid date
 	}
-	const day = String(date.getDate()).padStart(2, "0");
-	const month = String(date.getMonth() + 1).padStart(2, "0");
-	const year = date.getFullYear();
+    
+	// Extract date components
+	const day = String(date.getUTCDate()).padStart(2, "0");
+	const month = String(date.getUTCMonth() + 1).padStart(2, "0"); // January is 0, so we add 1
+	const year = date.getUTCFullYear();
+    
 	return `${day}-${month}-${year}`;
-}
+    }

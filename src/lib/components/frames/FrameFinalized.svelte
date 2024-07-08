22eac9e9ace3f18458881a6c9986e4c7b24f7cba
JOE IA <polilla2802@gmail.com>
2024-07-08 15:48:08 -0500

    Sound change final

--- src/lib/components/frames/FrameFinalized.svelte
+++ src/lib/components/frames/FrameFinalized.svelte
@@ -12,7 +12,7 @@
   let audio: HTMLAudioElement;
 
   function playSound(): void {
-    audio = new Audio("/sounds/page-3.mp3");
+    audio = new Audio("/sounds/page-4.mp3");
 
     if (audio) {
       audio.currentTime = 0; // Reset audio to start

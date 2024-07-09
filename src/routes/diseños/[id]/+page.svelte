f25992a58b699fac9a02edf343ccad05cfa8c82b
JOE IA <polilla2802@gmail.com>
2024-07-09 00:20:32 -0500

    Design access validation

--- src/routes/diseños/[id]/+page.svelte
+++ src/routes/diseños/[id]/+page.svelte
@@ -2,9 +2,15 @@
   import FrameDesign from "$lib/components/frames/FrameDesign.svelte";
   import { page } from "$app/stores"; // Import the page store from SvelteKit
   import { onMount } from "svelte"; // Optional: If you want to run some code when the component mounts
+  import { auth } from "$lib/firebase";
+  import type { User } from "firebase/auth";
   import Welcome from "$lib/components/messages/Welcome.svelte";
   import Icon from "@iconify/svelte";
+  import { getUserByUid } from "$utils/getUserByUid";
 
+  let currentUser: User | null = null; // Initialize currentUser to null
+  let userData: any;
+  
   // Use the `$page` store to get the data returned by the load function
   const { frameDesign } = $page.data.frameDesign;
   // Construct the base URL based on page url origin
@@ -14,11 +20,39 @@
 
   let notVisible: boolean;
 
+  function checkYourFrame(userData: any, frameDesign: any): boolean {
+    console.log(userData.user.id);
+    console.log(frameDesign.createdBy);
+    if (userData.user.id === frameDesign.createdBy) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
   // Optional: If you need to perform any action on mount
   onMount(() => {
     console.log("Component has mounted", frameDesign);
     if (frameDesign.isPublic == false) {
-      notVisible = true;
+      auth.onAuthStateChanged(async (authUser) => {
+        if (authUser) {
+          currentUser = authUser;
+          console.log("User is signed in:", currentUser);
+          if (currentUser != null) {
+            userData = await getUserByUid(currentUser!.uid);
+            notVisible = checkYourFrame(userData, frameDesign);
+          } else {
+            notVisible = true;
+            // If the user is not authenticated, you might want to handle it here
+            console.log("User is not authenticated");
+          }
+        } else {
+          notVisible = true;
+          console.log("No user signed in");
+        }
+      });
+    } else {
+      notVisible = false;
     }
   });
 </script>
